99d085d3dd90d8f25fbee1ea3bc30479f29bb1b2
theevex <96040977+theevex@users.noreply.github.com>
2023-03-17 20:33:09 +0700

    Update popup.component.ts

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -43,7 +43,7 @@ export class PopupComponent {
   createQr() {
     this.displaydivqr = 'block'
     this.displaydivpromtpay = 'none'
-    const mobileNumber = '***-***-****'
+    const mobileNumber = '095-594-1260'
     this.totalOrder = this.cvarrPrice[3]
     const amount = this.totalOrder
     const payload = generatePayload(mobileNumber, { amount })
